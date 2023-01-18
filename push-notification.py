309cd3ceb7c93f4cf2b4162e48c0d512109a0425
Yherald Bautista <YheraldBautista@users.noreply.github.com>
2023-01-18 14:09:31 +0100

    Adding some parameters - line comments

--- push-notification.py
+++ push-notification.py
@@ -1,15 +1,29 @@
+"""
+Pushover App test
+@author: Yherald Bautista
+"""
+# import modules
 import http.client, urllib
 
+# create connection
 conn = http.client.HTTPSConnection("api.pushover.net:443")
 file = 'resolution.txt'
 
+#Read a file with phrases
 with open(file, mode='r') as f:
     text = f.read()
 
+
+# make POST request to send message
 conn.request("POST", "/1/messages.json",
   urllib.parse.urlencode({
-    "token": "abc123",
-    "user": "user123",
+    "token": "token",
+    "user": "user",
+    "title": "PyPushNotification",
     "message": text,
+    "url": "",
+    "priority": "0" 
   }), { "Content-type": "application/x-www-form-urlencoded" })
+
+# get response
 conn.getresponse()
